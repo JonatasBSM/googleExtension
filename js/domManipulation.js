2cc5c424ac1953bbb53f0e775a940a51807eebb3
JonatasBSM <jonatasbsmcarvalho@gmail.com>
2022-10-14 07:40:55 -0300

    Avançando na extensão

--- js/domManipulation.js
+++ js/domManipulation.js
@@ -1,1 +1,11 @@
-window.open("www.youtube.com")
+var document = window.document
+
+var inputSerie = document.getElementById('osm_serie_0').value
+console.log(typeof(inputSerie))
+var inputNum = document.getElementById('osm_num_0').value
+
+if ((inputNum == null) || (inputSerie == null)) {
+    alert("Erro ao redirecionar para a NoPaper!")
+}
+
+window.open("http://portal.labapoiobrasil.com.br/osm/index-impressao-laudo/" + inputSerie.trim() + '/' + inputNum.trim(), '_blank');
